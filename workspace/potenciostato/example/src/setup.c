c1c2dc7980850bd0abe3272362301d2778cd5aef
Gustavo Arluna <gustavoarluna@gmail.com>
2021-09-04 18:49:38 -0300

    axel es gil-

--- workspace/potenciostato/example/src/setup.c
+++ workspace/potenciostato/example/src/setup.c
@@ -45,58 +45,58 @@ void prvSetupHardware(void)
     // Setup P0.2 SW_I_GAIN1
 	Chip_IOCON_PinMux(LPC_IOCON, PORT0, PIN(2), MD_PUP, IOCON_FUNC0);
     Chip_GPIO_SetPinDIROutput(LPC_GPIO, PORT0, PIN(2));
-    Chip_GPIO_SetPinOutLow(LPC_GPIO, PORT0, PIN(2));
+//    Chip_GPIO_SetPinOutLow(LPC_GPIO, PORT0, PIN(2));
 
     // Setup P0.3 SW_I_GAIN2
 	Chip_IOCON_PinMux(LPC_IOCON, PORT0, PIN(3), MD_PUP, IOCON_FUNC0);
     Chip_GPIO_SetPinDIROutput(LPC_GPIO, PORT0, PIN(3));
 //    Chip_GPIO_SetPinOutHigh(LPC_GPIO, PORT0, PIN(3));
-    Chip_GPIO_SetPinOutLow(LPC_GPIO, PORT0, PIN(3));
+//    Chip_GPIO_SetPinOutLow(LPC_GPIO, PORT0, PIN(3));
 
 
     // Setup P0.21 OFF_I_1
 	Chip_IOCON_PinMux(LPC_IOCON, PORT0, PIN(21), MD_PUP, IOCON_FUNC0);
     Chip_GPIO_SetPinDIROutput(LPC_GPIO, PORT0, PIN(21));
-    Chip_GPIO_SetPinOutHigh(LPC_GPIO, PORT0, PIN(21));
+//    Chip_GPIO_SetPinOutHigh(LPC_GPIO, PORT0, PIN(21));
 
     // Setup P0.22 OFF_I_2
 	Chip_IOCON_PinMux(LPC_IOCON, PORT0, PIN(22), MD_PUP, IOCON_FUNC0);
     Chip_GPIO_SetPinDIROutput(LPC_GPIO, PORT0, PIN(22));
 //    Chip_GPIO_SetPinOutHigh(LPC_GPIO, PORT0, PIN(22));
-    Chip_GPIO_SetPinOutLow(LPC_GPIO, PORT0, PIN(22));
+//    Chip_GPIO_SetPinOutLow(LPC_GPIO, PORT0, PIN(22));
 
     // Setup P2.1 SW_GAIN1 -  En el kickcad estaba al reves y era el P.2.2
 	Chip_IOCON_PinMux(LPC_IOCON, PORT2, PIN(1), MD_PUP, IOCON_FUNC0);
     Chip_GPIO_SetPinDIROutput(LPC_GPIO, PORT2, PIN(2));
-   Chip_GPIO_SetPinOutLow(LPC_GPIO, PORT2, PIN(2));
+//   Chip_GPIO_SetPinOutLow(LPC_GPIO, PORT2, PIN(2));
 
     // Setup P2.2 SW_GAIN2 -  En el kickcad estaba al reves y era el P.2.1
 	Chip_IOCON_PinMux(LPC_IOCON, PORT2, PIN(2), MD_PUP, IOCON_FUNC0);
     Chip_GPIO_SetPinDIROutput(LPC_GPIO, PORT2, PIN(1));
 //    Chip_GPIO_SetPinOutHigh(LPC_GPIO, PORT2, PIN(1));
-    Chip_GPIO_SetPinOutLow(LPC_GPIO, PORT2, PIN(1));
+//    Chip_GPIO_SetPinOutLow(LPC_GPIO, PORT2, PIN(1));
 
     // Setup P2.3 OFF_DAC_1
 	Chip_IOCON_PinMux(LPC_IOCON, PORT2, PIN(3), MD_PUP, IOCON_FUNC0);
     Chip_GPIO_SetPinDIROutput(LPC_GPIO, PORT2, PIN(3));
-    Chip_GPIO_SetPinOutHigh(LPC_GPIO, PORT2, PIN(3));
+//    Chip_GPIO_SetPinOutHigh(LPC_GPIO, PORT2, PIN(3));
 
     // Setup P2.4 OFF_DAC_2
 	Chip_IOCON_PinMux(LPC_IOCON, PORT2, PIN(4), MD_PUP, IOCON_FUNC0);
     Chip_GPIO_SetPinDIROutput(LPC_GPIO, PORT2, PIN(4));
 //    Chip_GPIO_SetPinOutHigh(LPC_GPIO, PORT2, PIN(4));
-    Chip_GPIO_SetPinOutLow(LPC_GPIO, PORT2, PIN(4));
+//    Chip_GPIO_SetPinOutLow(LPC_GPIO, PORT2, PIN(4));
 
     // Setup P2.5 SW_V_GAIN1
 	Chip_IOCON_PinMux(LPC_IOCON, PORT2, PIN(5), MD_PUP, IOCON_FUNC0); //Configuro la funcion del pin P2.5,Pull up, I/O
     Chip_GPIO_SetPinDIROutput(LPC_GPIO, PORT2, PIN(5));	//Configuro como salida el pi
-    Chip_GPIO_SetPinOutLow(LPC_GPIO, PORT2, PIN(5));
+//    Chip_GPIO_SetPinOutLow(LPC_GPIO, PORT2, PIN(5));
 
     // Setup P2.6 SW_V_GAIN2
 	Chip_IOCON_PinMux(LPC_IOCON, PORT2, PIN(6), MD_PUP, IOCON_FUNC0);
     Chip_GPIO_SetPinDIROutput(LPC_GPIO, PORT2, PIN(6));
 //    Chip_GPIO_SetPinOutHigh(LPC_GPIO, PORT2, PIN(6));
-    Chip_GPIO_SetPinOutLow(LPC_GPIO, PORT2, PIN(6));
+//    Chip_GPIO_SetPinOutLow(LPC_GPIO, PORT2, PIN(6));
 
     // Setup P2.7 OFF_V_1
 	Chip_IOCON_PinMux(LPC_IOCON, PORT2, PIN(7), MD_PUP, IOCON_FUNC0);
